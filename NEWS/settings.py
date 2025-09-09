d39572162d648c10c699ca14b72726c5bb222ed1
riya9818 <reeyabasnet999@gmail.com>
2025-09-09 22:19:18 +0545

    updated settings.py

--- NEWS/settings.py
+++ NEWS/settings.py
@@ -117,6 +117,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = ["static"]
+
+MEDIA_URL ="/media/"
+MEDIA_ROOT="media"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
