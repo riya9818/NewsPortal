ea5bde45b47d97a43331f8b122bb491ac40fac15
riya9818 <reeyabasnet999@gmail.com>
2025-09-11 21:13:12 +0545

    updated and registered admin.py with advertisements

--- newspaper/admin.py
+++ newspaper/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from newspaper.models import Category,Tag,Post
+from newspaper.models import Category,Tag,Post,Advertisement
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Tag)
 admin.site.register(Category)
+admin.site.register(Advertisement)
