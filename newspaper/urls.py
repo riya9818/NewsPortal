a0579d869530d6e108906e0b5e42699e066bf763
riya9818 <reeyabasnet999@gmail.com>
2025-09-14 19:28:56 +0545

    updated urls.py

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns =[
     path("",views.HomePageView.as_view(),name="home"),
     path("post-list/", views.PostListView.as_view(), name="post-list"),
+    path("post-detail/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
 ]
