19efb2fecad1dbb197881e52f7c50f392c425d75
riya9818 <reeyabasnet999@gmail.com>
2025-09-16 22:13:23 +0545

    updated urls.py

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -5,4 +5,5 @@ urlpatterns =[
     path("",views.HomePageView.as_view(),name="home"),
     path("post-list/", views.PostListView.as_view(), name="post-list"),
     path("post-detail/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
+    path("about/",views.AboutView.as_view(), name="about")
 ]
