86054b19beabbe5e3741f84f0b97d96e664445cf
riya9818 <reeyabasnet999@gmail.com>
2025-09-18 21:30:54 +0545

    form post with contact

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -5,6 +5,6 @@ urlpatterns =[
     path("",views.HomePageView.as_view(),name="home"),
     path("post-list/", views.PostListView.as_view(), name="post-list"),
     path("post-detail/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
-    path("about/",views.AboutView.as_view(), name="about")
+    path("about/",views.AboutView.as_view(), name="about"),
     path("contact/",views.ContactCreateView.as_view(), name="contact"),
 ]
