998238800ff56997d0ed8973a8829ba970f05748
riya9818 <reeyabasnet999@gmail.com>
2025-09-18 21:20:39 +0545

    urls updated with contact

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -6,4 +6,5 @@ urlpatterns =[
     path("post-list/", views.PostListView.as_view(), name="post-list"),
     path("post-detail/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
     path("about/",views.AboutView.as_view(), name="about")
+    path("contact/",views.ContactCreateView.as_view(), name="contact"),
 ]
