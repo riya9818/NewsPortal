068c90548ca2ae9c0a63bb6afc78074ced07c360
riya9818 <reeyabasnet999@gmail.com>
2025-09-21 21:11:23 +0545

    nav.py staff logic

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,4 @@ app_name = "accounts"
 urlpatterns=[
     path("login/",auth_views.LoginView.as_view(), name="login"),
     path("logout/",auth_views.LogoutView.as_view(), name="logout"),
-    path("register/",views.RegisterView.as_view(), name="register"),
 ]
