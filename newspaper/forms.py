457009b922ac849f0ab79de21e3c30339e71df8d
riya9818 <reeyabasnet999@gmail.com>
2025-09-22 20:05:27 +0545

    forms.py updated with comments

--- newspaper/forms.py
+++ newspaper/forms.py
@@ -1,8 +1,13 @@
 from django import forms
 
-from newspaper.models import Contact
+from newspaper.models import Contact, Comment
 
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = "__all__"
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["post","content"]
