94d236e185a362ab518dbe62fb4db74a25d5d8d3
riya9818 <reeyabasnet999@gmail.com>
2025-09-29 21:38:11 +0545

    tagserializers view

--- api/views.py
+++ api/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 from django.contrib.auth.models import Group, User
 from rest_framework import permissions, viewsets
 
-from api.serializers import GroupSerializer, UserSerializer
+from api.serializers import GroupSerializer, TagSerializer, UserSerializer
+from newspaper.models import Tag
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -22,4 +23,13 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     queryset = Group.objects.all().order_by('name')
     serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Tag.objects.all().order_by('name')
+    serializer_class = TagSerializer
     permission_classes = [permissions.IsAdminUser]
