f313c773bb017a1bf2a5affd0abcf285760c5c64
riya9818 <reeyabasnet999@gmail.com>
2025-09-29 22:00:37 +0545

    Updated serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,23 +1,84 @@
 from django.contrib.auth.models import Group, User
 from rest_framework import serializers
 
-from newspaper.models import Tag
+from newspaper.models import Category, Comment, Contact, Newsletter, Post, Tag
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields=["urls","username","email","groups"]
+        fields = ['id', 'username', 'email', 'groups', "first_name", "last_name"]
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
-        fields = ["url","name"]
+        fields = ['id', 'name']
 
-
+        
 class TagSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Tag
-        fields = ["id","name"]
+        fields = ["id", "name"]
+
         
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ["id", "name", "icon", "description"]\
+        
+
+
+class PostSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Post
+        fields = [
+            "id",
+            "title",
+            "content",
+            "featured_image",
+            "status",
+            "tag",
+            "category",
+
+            "author",
+            "views_count",
+            "published_at",
+        ]
+        extra_kwargs = {
+            "author": {"read_only": True},
+            "views_count": {"read_only": True},
+            "published_at": {"read_only": True},
+
+        }
+
+def validate(self, data):
+    data["author"] = self.context["request"].user
+    return data       
+
+class PostPublishSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ["id", "content", "created_at","post", "user"]
+        extra_kwargs = {
+            "post": {"read_only": True},
+            "user": {"read_only": True},
+            "created_at": {"read_only": True}
+        }
+
+class NewsletterSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Newsletter
+        fields = "__all__"
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        fields = "__all__"
