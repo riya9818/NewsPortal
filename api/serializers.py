c959bcba484aaf4ef3ee4e08acab94de62593d53
riya9818 <reeyabasnet999@gmail.com>
2025-09-29 19:59:06 +0545

    api made

---
+++ api/serializers.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import Group, User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields=["urls","username","email","groups"]
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        
